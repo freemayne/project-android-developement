baebdbf06ae167b0010f86a183eb1c83b19a7e2b
Semir Pecar <91013287+freemayne@users.noreply.github.com>
2022-11-21 13:14:42 +0100

    init

---
+++ components/customcomps/CustomInput.js
@@ -0,0 +1,59 @@
+import { StyleSheet, Text, View, TextInput } from "react-native"
+
+
+const CustomInput = ({label,inputConfig}) => {
+  
+ 
+  const inputStyles = [styles.input]
+  if (inputConfig && inputConfig.multiline){
+    inputStyles.push(styles.inputMultiline)
+  }
+
+  return (
+      <View>
+
+      <Text style={styles.label}>{label}</Text>
+    <View style={styles.inputContainer}>
+      <TextInput
+      style={inputStyles}
+      {...inputConfig} // gör att man kan konfigurera inputen i andra komponenter där man använder den.
+      />
+    </View>
+      </View>
+  )
+}
+
+export default CustomInput
+
+const styles = StyleSheet.create({
+  inputContainer:{
+    flexDirection:'row',
+    marginHorizontal:4,
+    
+    
+  }, 
+  input:{
+    marginLeft:20,
+    backgroundColor: "#ffffffa2",
+    padding: 10,
+    marginTop:10,
+    width: "90%",
+    borderRadius: 10,
+  },
+  label: {
+    color: 'black',
+    fontSize:18,
+    marginTop:12,
+    borderRadius:4,
+    textAlign:'center',
+    fontWeight:'bold'
+    
+
+  },inputMultiline:{
+    minHeight:100,
+    textAlignVertical:'top',
+    maxHeight:150,
+    
+  },
+
+})
